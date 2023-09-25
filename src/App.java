1af9734c2de8e929c51f89bec956e4d9b1ac0e38
deboralais <deboralais.arte@gail.com>
2023-09-25 18:38:34 -0300

    segundo commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vinda, turma DIO!");
+        Scanner entradaTeclado = new Scanner(System.in);
+        int parcela1, parcela2, soma;
+
+        System.out.print("Digite a 1a parcela - ");
+        parcela1 = entradaTeclado.nextInt();
+
+        System.out.print("Digite a 2a parcela - ");
+        parcela2 = entradaTeclado.nextInt();
+
+        soma = parcela1 + parcela2 ;
+
+        System.out.print("O resultado das parcelas é: " + soma);
+
+
     }
 }
